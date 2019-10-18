9a633c87fa3d9b8986c1a2eb984355cc7e566237
hanseungchul <schan@osci.kr>
2019-10-18 09:06:47 +0900

    ch5의 예제만 따로 분리, 전체 예제로 사용할 react-app 생성

---
+++ example/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import RefTest from './ch5/RefTest';
+
+ReactDOM.render( < RefTest / > , document.getElementById('root'));
