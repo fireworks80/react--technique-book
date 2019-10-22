84fc89db5fb53a82e1579900f24f463379abcf7d
hanseungchul <schan@osci.kr>
2019-10-23 08:48:33 +0900

    Add 삭제 기능 추가

--- example/src/ch6/IterationApplyClass.jsx
+++ example/src/ch6/IterationApplyClass.jsx
@@ -30,18 +30,38 @@ class IterationApplyClass extends Component {
     });
   };
 
+  handleRemoveListItem = (id) => {
+    // console.log(id);
+    //stateㅇㅔ서 해당 id 가 있는 아이를 제외한 나머지를 찾는다
+    // 찾은 요소들을 다시 state에 넣는다.
+    const { names } = this.state;
+
+    const filterdArr = names.filter((name, idx) => name.id !== id);
+    
+    this.setState({
+      names: filterdArr
+    });
+  };
+
+  handleKeyDown = (e) => {
+    // console.log(e.keyCode);
+    const ENTER = 13;
+
+    e.keyCode === ENTER && this.handleAddWord();
+  };
+
   render() {
-    const { state, handleChange, handleAddWord } = this;
+    const { state, handleChange, handleAddWord, handleRemoveListItem, handleKeyDown } = this;
     const { names, inputValue } = state;
 
     const namesList = names.map((name, idx) => (
-      <li key={name.id}>{name.text}</li>
+      <li key={name.id} onDoubleClick={() => handleRemoveListItem(name.id)}>{name.text}</li>
     ));
 
     return (
       <>
         <p>
-          <input onChange={handleChange} value={inputValue} type='text' />
+          <input onChange={handleChange} value={inputValue} onKeyDown={handleKeyDown} type='text' />
           <button onClick={handleAddWord}>add</button>
         </p>
         <ul>{namesList}</ul>
