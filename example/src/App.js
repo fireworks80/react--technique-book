9e873fd6aa70e4de93f0a8e850577e525a54dddc
hanseungchul <schan@osci.kr>
2019-11-07 08:25:12 +0900

    라이프 사이클 예제 완료

--- example/src/App.js
+++ example/src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import IterationApplyClass from './ch6/IterationApplyClass';
+import React, {
+  Component
+} from 'react';
+// import IterationApplyClass from './ch6/IterationApplyClass';
+import LifeCycle from './ch7/LifeCycle';
 
-function App() {
-  return <IterationApplyClass />;
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: '#000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
+  render() {
+    return ( 
+      <div>
+      <button onClick = {this.handleClick}>random color</button><LifeCycle color = {this.state.color}></LifeCycle>
+      </div>
+    );
+  }
 }
 
 export default App;
