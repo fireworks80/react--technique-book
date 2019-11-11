bd0a1b603d0c52dbd4ab4e4d6e68b19513e0da6d
hanseungchul <schan@osci.kr>
2019-11-11 09:56:14 +0900

    add ch8 기본 useState 예제 추가

--- example/src/App.js
+++ example/src/App.js
@@ -2,33 +2,38 @@ import React, {
   Component
 } from 'react';
 // import IterationApplyClass from './ch6/IterationApplyClass';
-import LifeCycle from './ch7/LifeCycle';
-import ErrorBoundary from './ch7/ErrorBoundary';
+// import LifeCycle from './ch7/LifeCycle';
+// import ErrorBoundary from './ch7/ErrorBoundary';
+import Counter from './ch8/Counter';
 
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-class App extends Component {
-  state = {
-    color: '#000'
-  }
+// class App extends Component {
+//   state = {
+//     color: '#000'
+//   }
 
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>random color</button>
-        <ErrorBoundary>
-          <LifeCycle color={this.state.color}></LifeCycle>
-        </ErrorBoundary> 
-      </div>
-    );
-  }
-}
+//   handleClick = () => {
+//     this.setState({
+//       color: getRandomColor()
+//     });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>random color</button>
+//         <ErrorBoundary>
+//           <LifeCycle color={this.state.color}></LifeCycle>
+//         </ErrorBoundary> 
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => { 
+  return <Counter />
+};
 
 export default App;
